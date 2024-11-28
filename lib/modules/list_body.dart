295ac3e1c24adae56fc676993a4c4b4948d37270
finteger <nashassociatesinc@gmail.com>
2024-11-28 14:24:16 -0700

    Update

--- lib/modules/list_body.dart
+++ lib/modules/list_body.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 List<String> generate() {
   List<String> imageList = [];
-  for (var i = 1; i < 8; i++) {
+  for (var i = 1; i < 9; i++) {
     imageList.add('assets/images/image${i}.jpg');
   }
   return imageList;
@@ -13,11 +13,9 @@ List<String> categories = generate();
 List<Widget> numbers = categories.map((category) {
   return Card(
     color: Colors.red,
-    child: Text(category,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 25,
-        )),
+    child: Image.asset(
+      category,
+      fit: BoxFit.cover,
+    ),
   );
 }).toList();
